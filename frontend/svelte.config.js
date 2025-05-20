dc09ff19bbf036c7c6f78cf69a2c704f3f058a67
Test User <test@example.com>
2025-05-20 11:43:02 -0700

    Add missing source files.

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -4,10 +4,16 @@ import adapter from '@sveltejs/adapter-static';
 const config = {
   kit: {
     adapter: adapter({
-      fallback: 'index.html'
+      // default options are shown
+      pages: 'build',
+      assets: 'build',
+      fallback: 'index.html',
+      precompress: false,
+      strict: true
     }),
-    prerender: {
-      handleMissingId: 'ignore'
+    paths: {
+      base: '',
+      assets: ''
     }
   }
 };
